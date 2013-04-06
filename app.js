ae578f056255cd2e30e8ac3ed6e8db0e529cfb62
Michael Victor Zink <mvz@mvz.so>
2013-04-06 16:27:00 +0000

    Fixing hard-coded domain in client socket connection

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 
 server.listen(3000);
 console.log('Listening on port 3000');
